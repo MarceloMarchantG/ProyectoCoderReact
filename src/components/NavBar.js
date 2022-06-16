537fca8c2669bd63d8413a41dbdf26c289056150
Marcelo Marchant <marcelo.marchant.g@gmail.com>
2022-06-16 14:49:48 -0400

    primer commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,45 @@
+import logo from '../img/logo-chico-removebg.png';
+import carro from '../img/cart.svg';
+import user from '../img/person.svg';
+import './NavBar.css';
+
+const NavBar = () => {
+    return(
+        <>
+        <nav className="App-NavBar">
+       
+            <div className='leftNavBar'>
+         
+                <a href="#inicio" >Inicio</a> 
+                <a href="#hamburguesas">Hamburguesas</a>
+                <a href="#starters">Starters</a>
+                <a href="#bebidas">Bebidas</a>
+            </div>
+            <div className='rightNavBar'>
+            <a href="#" role="button">
+                    <img src={user} alt="Usuario" />
+                </a>
+
+
+
+                <a href="sections/cart.html"  title="Ir al Carro">
+                    <img class="icon" src={carro} alt="Carrito"/>
+                </a>
+
+
+            </div>
+        </nav>    
+            <div className='title'>                           
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>ROD BURGER</h1>
+            </div>
+                    
+        
+        </>
+        )
+}
+
+export default NavBar
+
+
+
